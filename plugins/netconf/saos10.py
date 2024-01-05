48b492e66014d36c8e7a4f744a4b157a4180dcb2
Jeff Groom <jgroom@ciena.com>
2024-01-05 14:54:56 -0700

    lint

--- plugins/netconf/saos10.py
+++ plugins/netconf/saos10.py
@@ -1,3 +1,21 @@
+#
+# (c) 2017 Red Hat Inc.
+#
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+#
 from __future__ import absolute_import, division, print_function
 
 __metaclass__ = type
@@ -5,7 +23,7 @@ __metaclass__ = type
 DOCUMENTATION = """
 author:
   - Jeff Groom (@jgroom33)
-netconf: saos10
+name: saos10
 short_description: Use saos10 netconf plugin to run netconf commands on Ciena saos10
   platform
 description:
