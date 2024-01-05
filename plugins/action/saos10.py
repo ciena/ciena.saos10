2f3e3da8e5f1d6740de8b003e18b0758c55b5af0
Jeff Groom <jgroom@ciena.com>
2024-01-05 15:52:27 -0700

    simplify

--- plugins/action/saos10.py
+++ plugins/action/saos10.py
@@ -20,28 +20,12 @@ from __future__ import absolute_import, division, print_function
 
 __metaclass__ = type
 
-import sys
-import copy
-
 from ansible_collections.ansible.netcommon.plugins.action.network import (
     ActionModule as ActionNetworkModule,
-)
-from ansible_collections.ansible.netcommon.plugins.module_utils.network.common.utils import (
-    load_provider,
 )
 from ansible.utils.display import Display
 
 display = Display()
-
-CLI_SUPPORTED_MODULES = [
-    "saos10_netconf",
-    "saos10_ping",
-    "saos10_command",
-    "saos10_classifiers",
-    "saos10_fds",
-    "saos10_fps",
-]
-
 
 class ActionModule(ActionNetworkModule):
     def run(self, tmp=None, task_vars=None):
@@ -53,33 +37,9 @@ class ActionModule(ActionNetworkModule):
         warnings = []
 
         if persistent_connection in ("netconf", "network_cli"):
-            provider = self._task.args.get("provider", {})
-            if any(provider.values()):
-                if not (module_name == "saos10_facts"):
-                    display.warning(
-                        "provider is unnecessary when using %s and will be ignored"
-                        % self._play_context.connection
-                    )
-                    del self._task.args["provider"]
-
-            if (
-                persistent_connection == "network_cli"
-                and module_name not in CLI_SUPPORTED_MODULES
-            ) or (
-                persistent_connection == "netconf"
-                and module_name in CLI_SUPPORTED_MODULES[0:2]
-            ):
-                return {
-                    "failed": True,
-                    "msg": "Connection type '%s' is not valid for '%s' module. "
-                    "Please see https://docs.ansible.com/ansible/latest/network/user_guide/platform_junos.html"
-                    % (self._play_context.connection, module_name),
-                }
-        else:
             return {
                 "failed": True,
-                "msg": "Connection type %s is not valid for this module"
-                % self._play_context.connection,
+                "msg": "Connection type %s is not valid for this module" % self._play_context.connection,
             }
 
         result = super(ActionModule, self).run(task_vars=task_vars)
