7df4b99f9a7c17451a8ac1db09097b078bd85c48
Jeff Groom <jgroom@ciena.com>
2024-01-08 15:18:56 -0700

    fix fps facts

--- plugins/module_utils/network/saos10/facts/fps/fps.py
+++ plugins/module_utils/network/saos10/facts/fps/fps.py
@@ -9,11 +9,16 @@ It is in this file the configuration is collected from the device
 for a given resource, parsed, and the facts tree is populated
 based on the configuration.
 """
+from __future__ import absolute_import, division, print_function
+
+
+__metaclass__ = type
+
+
 from copy import deepcopy
 
-import re
-from ansible.module_utils._text import to_text, to_bytes
-from ansible.module_utils.basic import missing_required_lib
+import re  # pylint: disable=unused-import
+from ansible.module_utils._text import to_bytes
 from ansible_collections.ansible.netcommon.plugins.module_utils.network.common.netconf import (
     remove_namespaces,
 )
@@ -28,21 +33,13 @@ from ansible_collections.ciena.saos10.plugins.module_utils.network.saos10.argspe
 )
 
 try:
-    from lxml import etree
     from lxml.etree import tostring as xml_to_string, fromstring
 
     HAS_LXML = True
 except ImportError:
-    from xml.etree.ElementTree import fromstring, tostring as xml_to_string
+    from xml.etree.ElementTree import tostring as xml_to_string, fromstring
 
     HAS_LXML = False
-
-try:
-    import xmltodict
-
-    HAS_XMLTODICT = True
-except ImportError:
-    HAS_XMLTODICT = False
 
 
 class FpsFacts(object):
@@ -70,10 +67,12 @@ class FpsFacts(object):
         :rtype: dictionary
         :returns: facts
         """
+        if not HAS_LXML:
+            self._module.fail_json(msg="lxml is not installed.")
 
         if not data:
             config_filter = """
-                <fps>
+                <fps xmlns="urn:ciena:params:xml:ns:yang:ciena-pn::ciena-mef-fp">
                 </fps>
                 """
             data = get(self._module, filter=("subtree", config_filter))
@@ -81,8 +80,7 @@ class FpsFacts(object):
         stripped = remove_namespaces(xml_to_string(data))
         data = fromstring(to_bytes(stripped, errors="surrogate_then_replace"))
 
-        resources = data.xpath("/data/fps/fp")
-
+        resources = data.xpath("/rpc-reply/data/fps/fps")
         objs = []
         for resource in resources:
             if resource:
@@ -100,6 +98,23 @@ class FpsFacts(object):
         ansible_facts["ansible_network_resources"].update(facts)
         return ansible_facts
 
+    def get_xml_value(self, xml_obj, xpath):
+        result = xml_obj.xpath(xpath)
+        return result[0].text if result else None
+
+    def recursive_config_fill(self, config, conf, spec, xml_base_path=""):
+        for key, unused in spec.items():
+            modified_key = key.replace("_", "-")
+            new_base_path = f"{xml_base_path}/{modified_key}" if xml_base_path else modified_key
+
+            if isinstance(spec[key], dict):
+                config[key] = {}
+                self.recursive_config_fill(config[key], conf, spec[key], new_base_path)
+            else:
+                extracted_value = self.get_xml_value(conf, new_base_path)
+                if extracted_value is not None:
+                    config[key] = extracted_value
+
     def render_config(self, spec, conf):
         """
         Render config as dictionary structure and delete keys
@@ -110,22 +125,8 @@ class FpsFacts(object):
         :rtype: dictionary
         :returns: The generated config
         """
-        config = deepcopy(spec)
-        fp = self._get_xml_dict(conf)["fp"]
-        config["name"] = utils.get_xml_conf_arg(conf, "name")
-        if "logical-port" in fp:
-            config["logical-port"] = re.sub(
-                "^[a-z]+\:", "", fp["logical-port"]
-            )  # regex to remove namespace declaration in values
-        if "fd-name" in fp:
-            config["fd-name"] = re.sub(
-                "^[a-z]+\:", "", fp["fd-name"]
-            )  # regex to remove namespace declaration in values
-
+        if isinstance(conf, str):
+            conf = fromstring(conf)
+        config = {}
+        self.recursive_config_fill(config, conf, spec)
         return utils.remove_empties(config)
-
-    def _get_xml_dict(self, xml_root):
-        if not HAS_XMLTODICT:
-            self._module.fail_json(msg=missing_required_lib("xmltodict"))
-        xml_dict = xmltodict.parse(etree.tostring(xml_root), dict_constructor=dict)
-        return xml_dict
